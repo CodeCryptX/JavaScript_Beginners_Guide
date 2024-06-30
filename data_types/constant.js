3bacb53a836df0c066bcd84a60564180abbd37ed
HASSAN MURTAZA <hassan156013@gmail.com>
2024-06-30 12:35:25 -0700

    Create constant.js

---
+++ data_types/constant.js
@@ -0,0 +1,13 @@
+// ╔════════════════════╗
+// ║       Constants    ║
+// ╚════════════════════╝
+
+const firstName = "Jason";   // Define a constant firstName with value 'Jason'
+// firstName = "Hassan";     // This line will cause a TypeError because constants cannot be reassigned
+
+// Constants in JavaScript (const):
+// ╔════════════════════════════════════════════════════════════════════════════╗
+// ║ Constants are declared using the const keyword. Once initialized,a constant║
+// ║ cannot be reassigned a new value. Attempting to do so will result in a     ║
+// ║ TypeError.                                                                 ║
+// ╚════════════════════════════════════════════════════════════════════════════╝
